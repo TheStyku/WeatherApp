7b5f3dde1f7c9809a3f56d1f75c2f0ac93d6a606
Patryk Styczyski <patrykszu1994@o2.pl>
2022-10-08 21:46:34 +0200

    fix

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -7,7 +7,8 @@ function FrontPage({city_name, setCity_Name, fetchAsyncName, fetchAsyncLocal}) {
     <div className='front_tittle'>
         <h1 >14 dniowa prognoza pogody</h1>
         <p>Wyszukaj pogode po nazwie: </p>
-        <Form className='front_form' city_name={city_name} setCity_Name={setCity_Name} fetchAsync={fetchAsyncName}/>
+        <div className='front_form' ><Form city_name={city_name} setCity_Name={setCity_Name} fetchAsync={fetchAsyncName}/></div>
+        
         <p>Lub znajdz po swojej lokalizacji <button className='front_button' onClick={fetchAsyncLocal}> <MdMyLocation/></button></p>      
     </div>
   )
