c398ec742a8a694744799c2fb526485d49575ef9
Patryk Styczyski <patrykszu1994@o2.pl>
2022-10-15 11:50:07 +0200

    color-change

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import * as Icon from "react-icons/wi";
+import {HiSun} from 'react-icons/hi'
 
 function Icons(props) {
     if (props.weather === 'rain') {
-        return <Icon.WiRain className='Weather_icon' />
+        return <Icon.WiRain className='Weather_icon sky' />
     } else if (props.weather === 'clear-day') {
-        return <Icon.WiDaySunny className='Weather_icon' />
+        return <HiSun className='Weather_icon sunny' />
     } else if (props.weather === 'cloudy') {
-        return <Icon.WiCloud className='Weather_icon' />
+        return <Icon.WiCloud className='Weather_icon sky'/>
     } else if (props.weather === 'fog') {
-        return <Icon.WiFog className='Weather_icon' />
+        return <Icon.WiFog className='Weather_icon sky'/>
     } else if (props.weather === 'partly-cloudy-day') {
-        return <Icon.WiDayCloudy className='Weather_icon' />
+        return <Icon.WiDayCloudy className='Weather_icon sky'/>
     } else if (props.weather === 'rain-snow-showers-day') {
-        return <Icon.WiDayShowers className='Weather_icon' />
+        return <Icon.WiDayShowers className='Weather_icon sky'/>
     } else if (props.weather === 'rain-snow') {
-        return <Icon.WiRainMix className='Weather_icon' />
+        return <Icon.WiRainMix className='Weather_icon sky'/>
     } else if (props.weather === 'sleet') {
-        return <Icon.WiDaySleet className='Weather_icon' />
+        return <Icon.WiDaySleet className='Weather_icon sky'/>
     } else if (props.weather === 'snow') {
-        return <Icon.WiSnow className='Weather_icon' />
+        return <Icon.WiSnow className='Weather_icon sky'/>
     } else if (props.weather === 'thunder-rain') {
-        return <Icon.WiDayThunderstorm className='Weather_icon' />
+        return <Icon.WiDayThunderstorm className='Weather_icon sky'/>
     } else if (props.weather === 'thunder') {
-        return <Icon.WiDayThunderstorm className='Weather_icon' />
+        return <Icon.WiDayThunderstorm className='Weather_icon sunny' />
     } else {
         return <Icon.WiCloud />
     }
